98bdd4b089fff0b2f643459a10212c81f0d732b7
dahabu saidi <dahabusaidi@gmail.com>
2019-03-30 12:50:55 +0300

    added layouts

--- pages/index.php
+++ pages/index.php
@@ -5,3 +5,28 @@
  * Date: 3/30/19
  * Time: 12:18 PM
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HNG-Intern</title>
+    <link type="text/css" rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="../styleSheets/index.css">
+    <script rel="script" type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="row">
+    <div class="col-md-2">
+        <div class="side-nav">
+            
+        </div>
+    </div>
+    <div class="col-md-10">
+        <div class="content">
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
