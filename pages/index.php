3dbfbe23c1ba36e6fd03c36186c193a257108f7c
dahabu saidi <dahabusaidi@gmail.com>
2019-03-30 17:12:11 +0300

    added links

--- pages/index.php
+++ pages/index.php
@@ -14,17 +14,40 @@
     <link type="text/css" rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="../styleSheets/index.css">
     <script rel="script" type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+
+    <style>
+        
+    </style>
 </head>
 <body>
-<div class="row">
-    <div class="col-md-2">
-        <div class="side-nav">
-            
-        </div>
-    </div>
-    <div class="col-md-10">
-        <div class="content">
 
+<div class="col-md-10 offset-1 main">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="side-nav">
+                <div class="image-body">
+                    <h4>Dahabu Saidi</h4>
+                    <label for="image"></label>
+                    <img src="../images/IMG_4538.jpg" id="image" class="image" alt="No image" >
+                </div>
+
+                <ul class="links">
+                    <li style="list-style: none">
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="content">
+
+            </div>
         </div>
     </div>
 </div>
