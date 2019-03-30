fb81637714d2650b0a93210fe0118f81f06dddcf
dahabu saidi <dahabusaidi@gmail.com>
2019-03-30 17:30:40 +0300

    added social network panel

--- pages/index.php
+++ pages/index.php
@@ -16,33 +16,60 @@
     <script rel="script" type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
 
     <style>
-        
+        .links{
+            list-style-type: none;
+            text-align: center;
+            padding: 1px;
+            margin-top: 15px;
+            font-size: small;
+        }
+        .links li{
+            border-bottom: 1px solid #E3E1E1;
+            background-color: #F7F5F5;
+            margin-top: 10px;
+            padding: 5px;
+            border-radius: 4px;
+        }
+        .social{
+            list-style-type: none;
+            font-size: small;
+            font-style: italic;
+        }
     </style>
 </head>
 <body>
 
 <div class="col-md-10 offset-1 main">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-3" style="background-color: whitesmoke">
             <div class="side-nav">
                 <div class="image-body">
                     <h4>Dahabu Saidi</h4>
                     <label for="image"></label>
                     <img src="../images/IMG_4538.jpg" id="image" class="image" alt="No image" >
                 </div>
-
-                <ul class="links">
-                    <li style="list-style: none">
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Blog</a>
-                    </li>
-                    <li>
-                        <a href="#">Projects</a>
-                    </li>
-                </ul>
             </div>
+            <ul class="links">
+                <li style="list-style: none">
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">Projects</a>
+                </li>
+            </ul>
+
+            <ul class="social">
+                <li>social networks</li>
+                <li>
+                    <a></a>
+                    <a></a>
+                    <a></a>
+                    <a></a>
+                </li>
+            </ul>
         </div>
         <div class="col-md-9">
             <div class="content">
